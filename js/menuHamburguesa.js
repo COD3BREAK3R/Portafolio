e55e7b91cd31d297ad71f03c8ca1be0febbe3b78
COD3BREAK3R <hackermatrx1@gmail.com>
2024-05-29 12:21:51 -0400

    Navbar Creada

---
+++ js/menuHamburguesa.js
@@ -0,0 +1,35 @@
+const hamburguesa = document.querySelector(".hamburguesa");
+const oscurecer_fondo = document.querySelector(".oscurecer_fondo");
+const enlaces_nav_movil = document.querySelector(".enlaces_nav_movil");
+
+/*=============================================
+=            MARK:Menú Hamburguesa                  
+=============================================*/
+
+hamburguesa.addEventListener("click", () => {
+    hamburguesa.classList.toggle("activo");
+    oscurecer_fondo.classList.toggle("mostrar");
+    enlaces_nav_movil.classList.toggle("animar");
+
+    setTimeout(() => {}, 100);
+    document.body.classList.toggle("noScroll");
+});
+
+/*=============================================
+=       MARK:Posicionamiento Menú Movil                  
+=============================================*/
+
+const nav = document.querySelector("nav");
+const main = document.querySelector('main');;
+
+const calcularAlturaOscurecerFondo = () => {
+    const alturaNav = nav.offsetHeight;
+    main.style.marginTop = `${alturaNav}px`;
+    enlaces_nav_movil.style.top = `${alturaNav + 15}px`;
+};
+
+
+window.addEventListener("load", calcularAlturaOscurecerFondo);
+window.addEventListener("resize", calcularAlturaOscurecerFondo);
+
+/*============  End of Posicionamiento Menú Movil  =============*/

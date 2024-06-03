fdb98c5af3e8f315e001ec3a15646aa86de6fdf8
COD3BREAK3R <hackermatrx1@gmail.com>
2024-06-03 17:42:58 -0400

    Portafolio Creado

---
+++ js/tarjetasPortafolio.js
@@ -0,0 +1,64 @@
+/*=============================================
+=         MARK:Variables de Elementos                  
+=============================================*/
+
+const btnLeerMas = document.querySelectorAll(".leerMas");
+const btnVolverAtras = document.querySelectorAll(".volver_atras");
+// ------------------------------------------
+const portafolio_card = document.querySelectorAll(".portafolio_card");
+const portafolio_header = document.querySelectorAll(".portafolio_header");
+const portafolio_tecnologias = document.querySelectorAll(
+    ".portafolio_tecnologias"
+);
+const portafolio_botones = document.querySelectorAll(".portafolio_botones");
+// ------------------------------------------
+const portafolio_back = document.querySelectorAll(".portafolio_back");
+const contenido_back = document.querySelectorAll(".contenido_back");
+
+/*=============================================
+=                MARK:Funciones                  
+=============================================*/
+
+invertirCard = (indice) => {
+    portafolio_card[indice].style.transform = "rotateY(-180deg)";
+
+    portafolio_header[indice].style.visibility = "hidden";
+    portafolio_tecnologias[indice].style.visibility = "hidden";
+    portafolio_botones[indice].style.visibility = "hidden";
+
+    setTimeout(() => {
+        portafolio_back[indice].style.opacity = "1";
+        portafolio_back[indice].style.visibility = "visible";
+        contenido_back[indice].style.visibility = "visible";
+    }, 199);
+};
+
+volverAtras = (indice) => {
+    portafolio_card[indice].style.transform = "rotateY(0deg)";
+
+    setTimeout(() => {
+        portafolio_header[indice].style.visibility = "visible";
+        portafolio_botones[indice].style.visibility = "visible";
+        portafolio_tecnologias[indice].style.visibility = "visible";
+        contenido_back[indice].style.visibility = "hidden";
+    }, 199);
+
+    portafolio_back[indice].style.opacity = "0";
+    portafolio_back[indice].visibility = "hidden";
+};
+
+/*=============================================
+=           MARK:Event Listeners                   
+=============================================*/
+
+btnLeerMas.forEach((element, indice) => {
+    element.addEventListener("click", () => {
+        invertirCard(indice);
+    });
+});
+
+btnVolverAtras.forEach((element, indice) => {
+    element.addEventListener("click", () => {
+        volverAtras(indice);
+    });
+});
